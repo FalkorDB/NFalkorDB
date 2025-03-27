7b65ba8d6a122827193511564d8b48b7146642f1
Avi Avni <avi.avni@gmail.com>
2025-03-27 16:45:46 +0200

    clean

--- NFalkorDB.Tests/DatabaseExtensions.cs
+++ NFalkorDB.Tests/DatabaseExtensions.cs
@@ -21,37 +21,5 @@ namespace NFalkorDB.Tests
             _muxr.Dispose();
             _muxr = null;
         }
-
-        [Fact]
-        public void CanLolWut()
-        {
-            var result = _db.LolWut();
-            
-            Assert.NotNull(result.ToString());
-        }
-
-        [Fact]
-        public void CanLolWutWithParams()
-        {
-            var result = _db.LolWut(1, 2, 3, 4, 5);
-            
-            Assert.NotNull(result.ToString());
-        }
-
-        [Fact]
-        public async Task CanLolWutAsync()
-        {
-            var result = await _db.LolWutAsync();
-            
-            Assert.NotNull(result.ToString());
-        }
-
-        [Fact]
-        public async Task CanLolWutWithParamsAsync()
-        {
-            var result = await _db.LolWutAsync(1, 2, 3, 4, 5);
-            
-            Assert.NotNull(result.ToString());
-        }
     }
 }
